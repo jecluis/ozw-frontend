0f32f01b1397f49fc30331c0dcda7b79e3d689ce
Joao Eduardo Luis <joao@wipwd.dev>
2020-09-03 08:51:38 +0000

    node-details-table: adjust indentation
    Yes, we are doing this. Go hate somewhere else :)
    
    Signed-off-by: Joao Eduardo Luis <joao@wipwd.dev>

--- src/app/details-drawer/node-details-table/node-details-table-datasource.ts
+++ src/app/details-drawer/node-details-table/node-details-table-datasource.ts
@@ -28,97 +28,99 @@ export interface NodeDetailsByScope {
 export class NodeDetailsTableDataSource
         extends DataSource<NodeDetailValue> {
 
-  paginator: MatPaginator;
-  sort: MatSort;
+	paginator: MatPaginator;
+	sort: MatSort;
 
-  node_details: NodeDetailValue[] = [];
-  private node_details_subject =
-        new BehaviorSubject<NodeDetailValue[]>([]);
+	node_details: NodeDetailValue[] = [];
+	private node_details_subject =
+		new BehaviorSubject<NodeDetailValue[]>([]);
 
-  constructor(private http: HttpClient) {
-    super();
-  }
+	constructor(private http: HttpClient) {
+		super();
+	}
 
-  /**
-   * Connect this data source to the table. The table will only update when
-   * the returned stream emits new items.
-   * @returns A stream of the items to be rendered.
-   */
-  connect(): Observable<NodeDetailValue[]> {
-    // Combine everything that affects the rendered data into one update
-    // stream for the data-table to consume.
-    const dataMutations = [
-      this.node_details_subject,
-      this.paginator.page,
-      this.sort.sortChange
-    ];
+	/**
+	 * Connect this data source to the table. The table will only update when
+	 * the returned stream emits new items.
+	 * @returns A stream of the items to be rendered.
+	 */
+	connect(): Observable<NodeDetailValue[]> {
+		// Combine everything that affects the rendered data into one update
+		// stream for the data-table to consume.
+		const dataMutations = [
+			this.node_details_subject,
+			this.paginator.page,
+			this.sort.sortChange
+		];
 
-    return merge(...dataMutations).pipe(map(() => {
-      return this.getPagedData(this.getSortedData([...this.node_details]));
-    }));
-  }
+		return merge(...dataMutations).pipe(map(() => {
+			return this.getPagedData(
+				this.getSortedData([...this.node_details]));
+		}));
+	}
 
-  /**
-   *  Called when the table is being destroyed. Use this function, to clean up
-   * any open connections or free any held resources that were set up during connect.
-   */
-  disconnect() {}
+	/**
+	 *  Called when the table is being destroyed. Use this function, to clean up
+	 * any open connections or free any held resources that were set up during connect.
+	 */
+	disconnect() {}
 
-  /**
-   * Paginate the data (client-side). If you're using server-side pagination,
-   * this would be replaced by requesting the appropriate data from the server.
-   */
-  private getPagedData(data: NodeDetailValue[]) {
-    const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
-    return data.splice(startIndex, this.paginator.pageSize);
-  }
+	/**
+	 * Paginate the data (client-side). If you're using server-side pagination,
+	 * this would be replaced by requesting the appropriate data from the
+	 * server.
+	 */
+	private getPagedData(data: NodeDetailValue[]) {
+		const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
+		return data.splice(startIndex, this.paginator.pageSize);
+	}
 
-  /**
-   * Sort the data (client-side). If you're using server-side sorting,
-   * this would be replaced by requesting the appropriate data from the server.
-   */
-  private getSortedData(data: NodeDetailValue[]) {
-    if (!this.sort.active || this.sort.direction === '') {
-      return data;
-    }
+	/**
+	 * Sort the data (client-side). If you're using server-side sorting,
+	 * this would be replaced by requesting the appropriate data from the
+	 * server.
+	 */
+	private getSortedData(data: NodeDetailValue[]) {
+		if (!this.sort.active || this.sort.direction === '') {
+			return data;
+		}
 
-    return data.sort((a, b) => {
-      const isAsc = this.sort.direction === 'asc';
-      switch (this.sort.active) {
-        case 'label': return compare(a.label, b.label, isAsc);
-        case 'units': return compare(a.units, b.units, isAsc);
-        default: return 0;
-      }
-    });
-  }
+		return data.sort((a, b) => {
+			const isAsc = this.sort.direction === 'asc';
+			switch (this.sort.active) {
+				case 'label': return compare(a.label, b.label, isAsc);
+				case 'units': return compare(a.units, b.units, isAsc);
+				default: return 0;
+			}
+		});
+	}
 
-  loadDetails(node_id: number, scope: string) {
-    /*
-    this.node_details = this._getDetails(node_id, scope);
-    this.node_details_subject.next(this.node_details);
-    */
+	loadDetails(node_id: number, scope: string) {
 
-    if (typeof node_id !== "number" || typeof scope !== "string" ||
-        node_id <= 0 || scope.length == 0) {
-      return;
-    }
+		if (typeof node_id !== "number" || typeof scope !== "string" ||
+			node_id <= 0 || scope.length == 0) {
+		return;
+		}
 
-    let endpoint = '/api/nodes/'+node_id+'/scope/'+scope;
-    let node_details =
-      this.http.get<NodeDetailValue[]>(endpoint)
-        .pipe(
-          catchError( () => merge([]) ),
-          finalize( () => console.log("got node scope"))
-        )
-        .subscribe( values => {
-          this.node_details = values;
-          console.log("values = ", values);
-          this.node_details_subject.next(this.node_details);
-        });
-  }
+		let endpoint = '/api/nodes/'+node_id+'/values/genre/'+scope;
+		let node_details =
+		this.http.get<NodeDetailValue[]>(endpoint)
+		.pipe(
+			catchError( () => merge([]) ),
+			finalize( () => console.log("got node scope"))
+		)
+		.subscribe( values => {
+			this.node_details = values;
+			console.log("values = ", values);
+			this.node_details_subject.next(this.node_details);
+		});
+	}
 }
 
-/** Simple sort comparator for example ID/Name columns (for client-side sorting). */
+/**
+ * Simple sort comparator for example ID/Name columns (for client-side
+ * sorting).
+ */
 function compare(a: string | number, b: string | number, isAsc: boolean) {
-  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+	return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
