485a932935954f12926f2ca07807d545f55508d7
Joao Eduardo Luis <joao@wipwd.dev>
2020-09-15 17:08:03 +0000

    topology: show real-time topology graph
    Signed-off-by: Joao Eduardo Luis <joao@wipwd.dev>

--- src/app/network/view/network-view.component.ts
+++ src/app/network/view/network-view.component.ts
@@ -1,6 +1,16 @@
+/*
+ * Copyright (C) 2020  Joao Eduardo Luis <joao@wipwd.dev>
+ *
+ * This file is part of wip:wd's openzwave backend (ozw-backend).
+ * ozw-backend is free software: you can redistribute it and/or modify it under
+ * the terms of the EUROPEAN UNION PUBLIC LICENSE v1.2, as published by the
+ * European Comission.
+ */
 import { Component, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject, BehaviorSubject, Subscription } from 'rxjs';
 import { Edge, Node as NgxNode } from '@swimlane/ngx-graph';
+import { NetworkNode } from 'src/app/nodes/table/nodes-table-datasource';
+import { NodesService, NodeNeighbors } from 'src/app/nodes/service/nodes-service.service';
 
 @Component({
   selector: 'app-network-view',
@@ -9,65 +19,137 @@ import { Edge, Node as NgxNode } from '@swimlane/ngx-graph';
 })
 export class NetworkViewComponent implements OnInit {
 
-	constructor() { }
+	constructor(private _nodes_svc: NodesService) { }
+
+	private _network_nodes: {[id: number]: NetworkNode[]} = {};
+	private _network_node_by_id: {[id: number]: NetworkNode} = {};
+	private _nodes_observer: BehaviorSubject<NetworkNode[]> =
+		this._nodes_svc.getNodes();
+	private _node_neighbors_observer: BehaviorSubject<NodeNeighbors> =
+		this._nodes_svc.getNodeNeighbors();
+	
+	private _nodes_observer_subscription?: Subscription = undefined;
+	private _node_neighbors_subscription?: Subscription = undefined;
 
 	ngOnInit(): void {
+		console.debug("network-view: on init");
+		this._node_neighbors_subscription =
+			this._node_neighbors_observer.subscribe(
+				this._createGraph.bind(this));
+		this._nodes_observer_subscription =
+			this._nodes_observer.subscribe(this._handleNodes.bind(this));
 	}
 
 	ngAfterViewInit() {
 		this.center$.next(true);
+		this.zoomToFit$.next(true);
+	}
+
+	ngOnDestroy() {
+		console.debug("network-view: destroy component");
+		if (this._node_neighbors_subscription) {
+			this._node_neighbors_subscription.unsubscribe();
+		}
+		if (this._nodes_observer_subscription) {
+			this._nodes_observer_subscription.unsubscribe();
+		}
 	}
 
 	center$: Subject<boolean> = new Subject();
+	update$: Subject<boolean> = new Subject();
+	zoomToFit$: Subject<boolean> = new Subject();
 
-	public links: Edge[] = this.testGetNeighbors();
-	public nodes: NgxNode[] = this.testGetNodes();
+	private edge_accounting: {[id: string]: boolean} = {};
+	public nodes: NgxNode[] = [];
+	public edges: Edge[] = [];
 
+	private _handleNodes(nodes: NetworkNode[]): void {
 
+		if (nodes.length <= Object.keys(this._network_nodes).length) {
+			return;
+		}
 
-	testGetNeighbors(): Edge[] {
-		return [
-			{
-				source: 'node1',
-				target: 'node2'
-			},
-			{
-				source: 'node1',
-				target: 'node3'
-			},
-			{
-				source: 'node1',
-				target: 'node4'
-			},
-			{
-				source: 'node2',
-				target: 'node3'
+		let new_nodes = [];
+		nodes.forEach( (node: NetworkNode) => {
+			if (node.id in this._network_nodes) {
+				return;
 			}
-		]
+			new_nodes.push({
+				id: `${node.id}`,
+				label: `node ${node.id}`
+			});
+			this._network_nodes[node.id] = [];
+			this._network_node_by_id[node.id] = node;
+		});
+
+		if (new_nodes.length > 0) {
+			this.nodes = [...this.nodes, ...new_nodes];
+			this.center$.next(true);
+			this.zoomToFit$.next(true);
+		}
 	}
 
-	testGetNodes(): NgxNode[] {
-		return [
-			{
-				id: 'node1',
-				label: 'Node 1',			  
-			},
-			{
-				id: 'node2',
-				label: 'Node 2'
-			},
-			{
-				id: 'node3',
-				label: 'Node 3'
-			},
-			{
-				id: 'node4',
-				label: 'Node 4'
+	private last_update: number = new Date(0).getTime();
+	private new_edges: Edge[] = [];
+
+	private _createGraph(node_neighbors: NodeNeighbors): void {
+		// return;
+		console.debug("network-view: create graph for ", node_neighbors);
+		if (!node_neighbors) {
+			return;
+		}
+
+		let nodeid: string = `${node_neighbors.node.id}`;
+		if (!(nodeid in this._network_nodes)) {
+			console.debug(`network-view: node ${nodeid} not found`);
+			return;
+		}
+
+		// let new_edges: Edge[] = [];
+		node_neighbors.neighbors.forEach( (neighbor) => {
+			let neighborid: string = `${neighbor.id}`;
+			if (!(neighborid in this._network_nodes)) {
+				console.debug(`network-view: neighbor ${neighborid} not found`);
+				return;
 			}
-		]
+
+			let edge_str_a = `${nodeid}/${neighborid}`;
+			let edge_str_b = `${neighborid}/${nodeid}`;
+			if (edge_str_a in this.edge_accounting) {
+				return;
+			}
+			this.edge_accounting[edge_str_a] = true;
+			this.edge_accounting[edge_str_b] = true;
+			this.new_edges.push({
+				source: nodeid,
+				target: neighborid
+			});
+		});
+		let now: number = new Date().getTime();
+		if (now - this.last_update < 500) { return; }
+		this.last_update = now;
+
+		if (this.new_edges.length == 0) { return; }
+
+		this.edges = [...this.edges, ...this.new_edges];
+		this.new_edges = [];
+		console.log("total edges: ", this.edges);
+		// this.update$.next(true);
+		this.center$.next(true);
+		this.zoomToFit$.next(true);
 	}
 
 	_onClick(nodestr: string) {
 		console.log("clicked node: ", nodestr);
+	}
+
+	public isController(nodeidstr: string) {
+		if (!(nodeidstr in this._network_node_by_id)) {
+			console.error(`unable to find node ${nodeidstr} in memory`);
+			return false;
+		}
+
+		let node: NetworkNode = this._network_node_by_id[nodeidstr];
+		return (node.capabilities.is_controller);
 	}
 }
--- src/app/nodes/service/nodes-service.service.ts
+++ src/app/nodes/service/nodes-service.service.ts
@@ -10,22 +10,33 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NetworkNode } from '../table/nodes-table-datasource';
 import { catchError, finalize } from 'rxjs/operators';
-import { merge, BehaviorSubject, interval } from 'rxjs';
+import { merge, BehaviorSubject, interval, Observable } from 'rxjs';
 import { NetworkService, NetworkState } from '../../network/service/network.service';
+
+export interface NodeNeighbors {
+	node: NetworkNode;
+	neighbors: NetworkNode[];
+}
+
+declare type NodeNeighborsMap = {[id:number]: NodeNeighbors};
+declare type NodesMap = {[id: number]: NetworkNode};
 
 @Injectable({
 	providedIn: 'root'
 })
 export class NodesService {
 
-	private _nodes_data: NetworkNode[] = [];
+	private _nodes_data: NodesMap = {};
 	private _nodes_subject = new BehaviorSubject<NetworkNode[]>([]);
+	private _nodes_neighbors: NodeNeighborsMap = {};
+	private _nodes_neighbors_subject =
+		new BehaviorSubject<NodeNeighbors>(undefined);
 	private _last_fetch: Date = new Date(0);
 	private _fetch_interval: number = 20000; // 20 seconds in miliseconds
 
 
 	constructor(
-		private http: HttpClient,
+		private _http: HttpClient,
 		private _network_svc: NetworkService
 	) {
 		interval(this._fetch_interval).subscribe( () => {
@@ -54,15 +65,19 @@ export class NodesService {
 
 	private _getNodes() {
 		let nodes =
-			this.http.get<NetworkNode[]>('/api/nodes')
+			this._http.get<NetworkNode[]>('/api/nodes')
 		.pipe(
 			catchError( () => merge([]) ),
 			finalize( () => console.debug("nodes-svc: successfully got nodes"))
 		)
-		.subscribe( nodes => {
-			this._nodes_data = nodes;
-			this._nodes_subject.next(this._nodes_data);
+		.subscribe( (nodes: NetworkNode[]) => {
+			this._nodes_data = {};
+			nodes.forEach( (node: NetworkNode) => {
+				this._nodes_data[node.id] = node;
+			});
+			this._nodes_subject.next(nodes);
 			console.debug("nodes-svc: nodes =", nodes);
+			this._updateNodeNeighbors();
 		});
 	}
 
@@ -81,6 +96,34 @@ export class NodesService {
 		}
 	}
 
+	private _getNodeNeighbors(neighbors: number[]): NetworkNode[] {
+		let lst: NetworkNode[] = [];
+
+		neighbors.forEach( (id: number) => {
+			if (id in this._nodes_data) {
+				lst.push(this._nodes_data[id]);
+			}
+		});
+		return lst;
+	}
+
+	private _updateNodeNeighbors() {
+		Object.keys(this._nodes_data).forEach( (idstr: string) => {
+			this._http.get<number[]>(`/api/nodes/${idstr}/neighbors`)
+			.pipe(
+				catchError( () => merge([]))
+			)
+			.subscribe( (neighbors: number[]) => {
+
+				let _node_neighbors: NodeNeighbors = {
+					node: this._nodes_data[+idstr],
+					neighbors: this._getNodeNeighbors(neighbors)
+				};
+				this._nodes_neighbors_subject.next(_node_neighbors);
+			});
+		});
+	}
+
 
 	public getNodes(): BehaviorSubject<NetworkNode[]> {
 		this._fetchInfo();
@@ -91,5 +134,17 @@ export class NodesService {
 		this._nodes_data = [];
 		this._nodes_subject.next([]);
 	}
-  
+
+	public getNodeNeighbors(): BehaviorSubject<NodeNeighbors> {
+		this._fetchInfo();
+		return this._nodes_neighbors_subject;
+	}
+
+	public getNodeById(id: number): NetworkNode {
+		return this._nodes_data[id];
+	}
+
+	public nodeExists(id: number): boolean {
+		return (id in this._nodes_data);
+	}
 }
