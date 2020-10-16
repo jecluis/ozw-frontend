faea3acc9d237c74d62d3582f522c7bb5e69f323
Joao Eduardo Luis <joao@wipwd.dev>
2020-10-16 12:15:23 +0000

    config: add config interface
    And do some cleanup, make tabs into spaces.
    
    Signed-off-by: Joao Eduardo Luis <joao@wipwd.dev>

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -1,53 +1,81 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { config, BehaviorSubject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 export interface Configuration {
-	driver: {
-		device: string;
-		autostart: boolean;
-	},	
-	prometheus: {
-		url: string
-	}
+    http: {
+        host: string;
+        port: number;
+    };
+    zwave: {
+        device: string;
+    };
+    prometheus: {
+        url: string;
+    };
+}
+
+interface APIConfigItem {
+    config: Configuration;
 }
 
 @Injectable({
-	providedIn: 'root'
+    providedIn: 'root'
 })
 export class ConfigService {
 
-	private _config_test: Configuration = {
-		driver: {
-			device: '/dev/foo',
-			autostart: false
-		},
-		prometheus: {
-			url: 'http://123.456.123.232'
-		}
-	}
-	private _config = new BehaviorSubject<Configuration>(undefined);
+    // private _config_test: Configuration = {
+    //     driver: {
+    //         device: '/dev/foo',
+    //         autostart: false
+    //     },
+    //     prometheus: {
+    //         url: 'http://123.456.123.232'
+    //     }
+    // };
+    private _config: Configuration;
+    private _config_observer: BehaviorSubject<Configuration> =
+        new BehaviorSubject<Configuration>({} as Configuration);
+    private _config_set_status_observer: BehaviorSubject<boolean> =
+        new BehaviorSubject<boolean>(false);
 
-	constructor() {
-		console.log("config-svc: construct!");
-		this._config.next(this._config_test);
-	}
 
+    constructor(private _http: HttpClient) {
+        this._obtainConfig();
+    }
 
-	public getConfig(): BehaviorSubject<Configuration> {
-		return this._config;
-	}
+    private _obtainConfig(): void {
+        this._http.get<APIConfigItem>("/api/config")
+        .subscribe(
+            (config: APIConfigItem) => {
+                if (Object.keys(config).length > 0) {
+                    this._config = config.config;
+                }
+                this._config_observer.next(this._config);
+            }
+        );
+    }
 
-	public setConfig(configstr: string): boolean {
-		let config: Configuration = undefined;
-		try {
-			config = JSON.parse(configstr);
-		} catch (e) {
-			return false;
-		}
-		this._config.next(config);
-		console.log('config-svc: set configuration');
-		return true;
-	}
-	
+    public getConfig(): BehaviorSubject<Configuration> {
+        return this._config_observer;
+    }
 
+    public setConfig(configstr: string): BehaviorSubject<boolean> {
+        let _config: Configuration = {} as Configuration;
+        try {
+            _config = JSON.parse(configstr);
+        } catch (e) {
+            this._config_set_status_observer.next(false);
+            return this._config_set_status_observer;
+        }
+        const api_config: APIConfigItem = { config: _config };
+        this._http.post<boolean>("/api/config", api_config)
+        .subscribe( (res: boolean) => {
+            if (res) {
+                this._obtainConfig();
+            }
+            this._config_set_status_observer.next(res);
+        });
+        return this._config_set_status_observer;
+    }
 }
--- src/app/config/view/config-view.component.ts
+++ src/app/config/view/config-view.component.ts
@@ -1,81 +1,123 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { FormGroup, FormControl, Validators, Validator, ValidatorFn, AbstractControl } from '@angular/forms';
+import {
+    FormGroup, FormControl, Validators, ValidatorFn, AbstractControl
+} from '@angular/forms';
 import { ConfigService, Configuration } from '../config.service';
 import { BehaviorSubject, Subscription } from 'rxjs';
 
 @Component({
-	selector: 'app-config-view',
-	templateUrl: './config-view.component.html',
-	styleUrls: ['./config-view.component.scss']
+    selector: 'app-config-view',
+    templateUrl: './config-view.component.html',
+    styleUrls: ['./config-view.component.scss']
 })
-export class ConfigViewComponent implements OnInit {
+export class ConfigViewComponent implements OnInit, OnDestroy {
 
-	constructor(
-		private _config_svc: ConfigService,
-		private _dialog: MatDialog
-	) { }
+    constructor(
+        private _config_svc: ConfigService,
+        private _dialog: MatDialog
+    ) { }
 
-	configtext_ctrl = new FormControl('', [Validators.required, isValidJSON()]);
-	configform = new FormGroup({
-		configtext: this.configtext_ctrl
-	});
+    public configtext_ctrl =
+        new FormControl('', [Validators.required, isValidJSON()]);
+    public configform = new FormGroup({
+        configtext: this.configtext_ctrl
+    });
 
-	private _config: Configuration = {} as Configuration;
-	private _config_subscription: Subscription =
-		this._config_svc.getConfig().subscribe( (config: Configuration) => {
-			this._config = config;
-			console.log(`config-view: new config: `, this._config);
-			this._updateConfig();
-		}
-	);
+    private _show_setting_error: boolean = false;
+    private _show_setting_success: boolean = false;
+    private _is_setting: boolean = false;
+    private _config: Configuration = {} as Configuration;
+    private _config_subscription: Subscription =
+        this._config_svc.getConfig().subscribe( (config: Configuration) => {
+            this._config = config;
+            console.log(`config-view: new config: `, this._config);
+            this._updateConfig();
+        }
+    );
+    private _set_subscription: Subscription;
 
-	ngOnInit(): void {
-		this._updateConfig();
-	}
+    ngOnInit(): void {
+        this._updateConfig();
+    }
 
-	ngOnDestroy(): void {
-		this._config_subscription.unsubscribe();
-	}
+    ngOnDestroy(): void {
+        this._config_subscription.unsubscribe();
+    }
 
-	private _updateConfig() {
-		console.log("config-view: update config");
-		this.configtext_ctrl.setValue(
-			JSON.stringify(this._config, null, '   '));
-	}
+    private _updateConfig(): void {
+        console.log("config-view: update config");
+        this.configtext_ctrl.setValue(
+            JSON.stringify(this._config, null, '   '));
+    }
 
-	public openConfirmDialog() {
-		const _dialog_ref = this._dialog.open(ConfigViewConfirmDialogComponent);
-		_dialog_ref.afterClosed().subscribe( (result: boolean) => {
-			if (!!result && result) {
-				this._confirmConfigChange();
-			}
-			console.log(`config-view: dialog result: ${result}`);
-		});
-	}
+    public isSettingConfig(): boolean {
+        return this._is_setting;
+    }
 
-	private _confirmConfigChange() {
-		let configstr: string = this.configform.value['configtext'];
-		console.log(`config-view: set config to ${configstr}`);
-		if (!this._config_svc.setConfig(configstr)) {
-			console.error("invalid configuration");
-		}
-	}
+    public showSettingError(): boolean {
+        return this._show_setting_error;
+    }
+
+    public showSettingSuccess(): boolean {
+        return this._show_setting_success;
+    }
+
+    public openConfirmDialog(): void {
+        const _dialog_ref = this._dialog.open(ConfigViewConfirmDialogComponent);
+        _dialog_ref.afterClosed().subscribe( (result: boolean) => {
+            if (!!result && result) {
+                this._confirmConfigChange();
+            }
+            console.log(`config-view: dialog result: ${result}`);
+        });
+    }
+
+    private _confirmConfigChange(): void {
+        const configstr: string = this.configform.value.configtext;
+        console.log(`config-view: set config to ${configstr}`);
+
+        this._show_setting_error = false;
+        this._show_setting_success = false;
+
+        this._set_subscription = this._config_svc.setConfig(configstr)
+        .subscribe(
+            (res: boolean) => {
+                console.log(`config-view: set result: ${res}`);
+                if (res) {
+                    this._show_setting_error = false;
+                    this._show_setting_success = true;
+                } else {
+                    this._show_setting_error = true;
+                    this._show_setting_success = false;
+                }
+                this._is_setting = false;
+                this._set_subscription?.unsubscribe();
+            },
+            (error) => {
+                this._show_setting_error = true;
+                this._show_setting_success = false;
+                this._is_setting = false;
+            });
+        if (!this._config_svc.setConfig(configstr)) {
+            console.error("invalid configuration");
+        }
+    }
 }
 
 function isValidJSON(): ValidatorFn {
-	return (control: AbstractControl): {[key: string]: any | null} => {
-		let is_valid_json: boolean = false;
-		try {
-			JSON.parse(control.value);
-			is_valid_json = true;
-		} catch (e) { }
-		return (is_valid_json ? null : { invalidJSON: {value: control.value}});
-	}
+    return (control: AbstractControl): {[key: string]: any | null} => {
+        let is_valid_json: boolean = false;
+        try {
+            JSON.parse(control.value);
+            is_valid_json = true;
+        } catch (e) { }
+        return (is_valid_json ? null : { invalidJSON: {value: control.value}});
+    }
 }
 
 @Component({
-	selector: 'app-config-view-confirm-dialog',
-	templateUrl: 'config-view-confirm-dialog.html'
+    selector: 'app-config-view-confirm-dialog',
+    templateUrl: 'config-view-confirm-dialog.html'
 })
 export class ConfigViewConfirmDialogComponent { }
