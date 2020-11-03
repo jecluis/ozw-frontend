c3575191fd62705c59ebe267bb4481a5b481e4c3
Joao Eduardo Luis <joao@wipwd.dev>
2020-11-03 14:50:16 -0100

    devices: make view reactive, prettier on handset
    Signed-off-by: Joao Eduardo Luis <joao@wipwd.dev>

--- src/app/devices/device-list/device-list.component.ts
+++ src/app/devices/device-list/device-list.component.ts
@@ -1,4 +1,7 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-device-list',
@@ -7,11 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeviceListComponent implements OnInit {
 
+    public isHandset$: Observable<boolean> =
+        this.breakpointObserver.observe([
+            Breakpoints.Handset,
+            // Breakpoints.Medium,
+            Breakpoints.Small
+        ]
+    )
+    .pipe(
+        map(result => result.matches),
+        shareReplay()
+    );
     public device_type: string = "all";
 
-    constructor() { }
+    private _is_handset: boolean = false;
 
-    ngOnInit(): void {
+    constructor(
+        private breakpointObserver: BreakpointObserver
+    ) {
+        this.isHandset$.subscribe({
+            next: (result: boolean) => {
+                this._is_handset = result;
+            }
+        });
+    }
+
+    public ngOnInit(): void {
     }
 
 }
